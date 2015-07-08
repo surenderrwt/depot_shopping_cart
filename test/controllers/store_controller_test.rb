30048ae46323db92c9ed009b971f0559f66ffeff
estebangallego <egallego00@gmail.com>
2015-07-07 23:04:16 -0700

    index_layout

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.container .col-md-2'
+    assert_select 'table'
+    assert_select 'h4', 'Your Pragmatic Catalog'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+    # select elements in the page for testing
   end
 
 end
